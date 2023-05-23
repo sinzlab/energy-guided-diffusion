5e76af9122c693bea28bbf0cd4461aa9b2a8a174
paul <ppierzc@gmail.copm>
2023-05-23 15:49:49 +0200

    fix(models): remove empty models dir, added to download_requirements.sh

--- download_requirements.sh
+++ download_requirements.sh
@@ -8,3 +8,6 @@ curl https://api.wandb.ai/artifactsV2/gcp-us/sinzlab/QXJ0aWZhY3Q6NDYzNDUyNzgw/02
 curl https://api.wandb.ai/artifactsV2/gcp-us/sinzlab/QXJ0aWZhY3Q6NDYzNDUyODA0/b6ba56e1f05dd3ef5dfa27d52262d1ef/75_monkey_test_imgs.npy > ./data/75_monkey_test_imgs.npy
 curl https://api.wandb.ai/artifactsV2/gcp-us/sinzlab/QXJ0aWZhY3Q6NDYzNDUyODI3/32ef7776f79ea9895c9df78ed294fe85/pretrained_resnet_unit_correlations.npy > ./data/pretrained_resnet_unit_correlations.npy
 curl https://api.wandb.ai/artifactsV2/gcp-us/sinzlab/QXJ0aWZhY3Q6NDYzNDUyODUz/5f7c67355d51a99d2f822f2015891420/data_driven_corr.npy > ./data/data_driven_corr.npy
+
+mkdir "models"
+curl https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_diffusion_uncond.pt > ./models/256x256_diffusion_uncond.pt
--- models/README.md
+++
@@ -1,1 +0,0 @@
-Place the pretrained model files here
