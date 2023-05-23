0db7a3ba5ae9aef66997143651faccfbaae3a4ed
paul <ppierzc@gmail.copm>
2023-05-23 15:37:12 +0200

    add(data): add downloading data requirements

--- download_requirements.sh
+++ download_requirements.sh
@@ -2,3 +2,9 @@ git clone https://github.com/sinzlab/mei ./lib/mei
 git clone https://github.com/dicarlolab/CORnet.git ./lib/CORnet
 git clone https://github.com/sacadena/ptrnets.git ./lib/ptrnets
 git clone -b model_builder https://github.com/sinzlab/nnvision.git ./lib/nnvision
+
+mkdir "data"
+curl https://api.wandb.ai/artifactsV2/gcp-us/sinzlab/QXJ0aWZhY3Q6NDYzNDUyNzgw/0241dae91ce4fc127ded0a1598a4f5bd/target_l2.npy > ./data/target_l2.npy
+curl https://api.wandb.ai/artifactsV2/gcp-us/sinzlab/QXJ0aWZhY3Q6NDYzNDUyODA0/b6ba56e1f05dd3ef5dfa27d52262d1ef/75_monkey_test_imgs.npy > ./data/75_monkey_test_imgs.npy
+curl https://api.wandb.ai/artifactsV2/gcp-us/sinzlab/QXJ0aWZhY3Q6NDYzNDUyODI3/32ef7776f79ea9895c9df78ed294fe85/pretrained_resnet_unit_correlations.npy > ./data/pretrained_resnet_unit_correlations.npy
+curl https://api.wandb.ai/artifactsV2/gcp-us/sinzlab/QXJ0aWZhY3Q6NDYzNDUyODUz/5f7c67355d51a99d2f822f2015891420/data_driven_corr.npy > ./data/data_driven_corr.npy
